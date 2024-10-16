07295ca204dab61a6b5132ff2faef30324f6b8a0
Rob-Yoo <jin12243@naver.com>
2024-10-16 14:16:20 +0900

    commit test

---
+++ movie/movie/MediaDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MediaDetailView.swift
+//  movie
+//
+//  Created by Jinyoung Yoo on 10/16/24.
+//
+
+import SwiftUI
+
+struct MediaDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MediaDetailView()
+}
